e0d5118751de5fde636646737ba4dca8e6c2786c
Luiza <luizahelena.lsantos@gmail.com>
2024-12-31 12:55:12 -0300

    error handling

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ const express = require('express');
 const pool = require('./db');
 const app = express();
 
-
-
-app.get('/monsters', (req, res) => {
-    pool.query('SELECT * FROM monsters;', (err, res) => {
-        if (err) return console.log(err);
-    
-        console.log(res).rows;
+app.get('/monsters', (request, response, next) => {
+    pool.query('SELECT * FROM tchcjhgchcg ORDER BY id ASC;', (err, res) => {
+        if (err)
+            return next(err);
+        response.json(res.rows);
     });
 });
 
-const port = 3000;
+// Error handling
+app.use((err, req, res, next) => {
+    res.json(err);
+})
 
-app.listen(port, () => console.log('Listening on port ' + port));
+module.exports = app;
